ff7ef9afa8cc5f339039ad94c06133ed987a9786
Kevin Luis Flórez Lozada <71282006+kevinluisfl@users.noreply.github.com>
2022-03-13 19:42:03 -0500

    Update App.js

--- frontend_contact/src/App.js
+++ frontend_contact/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Contacto from "./page/Contacto";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <Contacto />
-    </div>
+    <Router>
+      <Provider store={store}>
+        <Switch>
+         <Route exact path="/" component={Contacto} />
+          {/* <Contacto /> */}
+        </Switch>
+      </Provider>
+    </Router>
   );
 }
 
